4ca84ea7b64fe8f4111d324af89551ea864dc346
isain <ibarrais@msu.edu>
2022-08-08 13:47:26 -0400

    First commit containing code

---
+++ assets/script.js
@@ -0,0 +1,44 @@
+let questions = [{
+    question: "Arrays in JavaScript can be used to store which of the following?",
+    option1: "Variables",
+    option2: "Numbers",
+    option3: "Strings",
+    option4: "All of the above"},
+  {
+    question: "What does HTML stand for?",
+    option1: "HyperText Madeup Language",
+    option2: "Hypertext Markup Language",
+    option3: "HyperTell Markup Language",
+    option4: "HyperTall Madeup Language"}, 
+  {
+    question: "The condition in an If / else statement is enclosed with _________",
+    option1: "Curly brackets",
+    option2: "Quotes",
+    option3: "Square brackets",
+    option4: "Parentheses"},
+  {
+    Question: "What is the variable naming convention used in JavaScript called?",
+    option1: "camelCase",
+    option2: "snake_case",
+    option3: "Cobra_Case",
+    option4: "PascalCase"},
+  {
+    question: "Which line of javaScript code will generate a random number in between 1 and any integer n?",
+    option1: "Math.random() * n + 1",
+    option2: "Math.random(1,n)",
+    option3: "Math.floor(Math.random() * n) + 1",
+    option4: "Math.floor(Math.random(n)"},
+]
+
+
+
+
+
+
+
+
+function initializeGame() {
+
+}
+
+function ()
